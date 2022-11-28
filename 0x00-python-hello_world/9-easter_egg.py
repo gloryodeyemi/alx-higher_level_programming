ab7610f01cc816ca3e6fa2ed01691239b356c168
Glory Odeyemi <59374178+gloryodeyemi@users.noreply.github.com>
2022-11-28 15:46:12 -0800

    Create 9-easter_egg.py

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\n
+Simple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\n
+Readability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\n
+Errors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\n
+There should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\n
+Now is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\n
+If the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!")
