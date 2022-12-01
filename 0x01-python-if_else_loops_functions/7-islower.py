ae6b5017d8b8b0bb586beb0e308b88f965486178
Glory Odeyemi <59374178+gloryodeyemi@users.noreply.github.com>
2022-12-01 14:30:58 -0800

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == c.lower():
-        return True
-    else:
-        return False
+    if ord(c) >= 97 and ord(c) <= 177:
+        return (True)
+    return (False)
