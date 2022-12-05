20022584b9b1cf84a8325bd8f09847885d98bef8
Glory Odeyemi <59374178+gloryodeyemi@users.noreply.github.com>
2022-12-05 10:03:56 -0800

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,94 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-size_t list_len(listint_t *head);
+int len_list(listint_t *head)
+{
+	int i = 0;
+	listint_t *h = head;
 
-listint_t *add_node(listint_t **head, int num);
+	while (head != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
 
 /**
- * is_palindrome - Checks if a singly linked list is a palidrome
- * @head: The start of the list
- *
- * Return: 1 if is or 0 if not
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: singly linked list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *tmp_head = NULL;
-	listint_t *tmp_current = NULL;
-	size_t i = 0, len = list_len(*head);
+	int a, i;
+	listint_t *tmp = *head;
+	int array;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-	while (i < (len / 2))
+	l = len_list(*head);
+	array = malloc(sizeof(int) * l);
+
+	if (array == NULL)
+	        exit(-1);
+
+	i = 0;
+	while (temp != NULL)
 	{
-		add_node(&tmp_head, current->n);
-		current = current->next;
+		array[i] = temp->n;
+		temp = temp->next;
 		i++;
 	}
-	if ((len % 2) != 0)
-	{
-		current = current->next;
-		i++;
-	}
-	tmp_current = tmp_head;
-	while (i < len)
-	{
-		if (tmp_current->n == current->n)
-		{
-			current = current->next;
-			tmp_current = tmp_current->next;
-		}
-		else
-		{
-			free_listint(tmp_head);
-			return (0);
-		}
-		i++;
-	}
-	free_listint(tmp_head);
-	return (1);
-}
 
-/**
- * list_len - Goes through a listint_t list
- * @head: The first node of the list
- *
- * Return: Always an unsigned int, the total number of nodes
- */
-size_t list_len(listint_t *head)
-{
-	unsigned int total = 0;
-	listint_t *current = head;
-
-	if (head == NULL)
-		return (0);
-
-	while (current)
-	{
-		total++;
-		current = current->next;
-	}
-
-	return (total);
-}
-
-/**
- * add_node - Adds a new node at the beginning of a list
- * @head: The first node of a list
- * @num: The integer for the new node
- *
- * Return: The address of the new node or NULL
- */
-listint_t *add_node(listint_t **head, int num)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = num;
-	new->next = *head;
-	*head = new;
-
-	return (*head);
+	
 }
