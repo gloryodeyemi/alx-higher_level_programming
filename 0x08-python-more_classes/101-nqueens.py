36d89ecc0d4aaf76806756705525e41f2b8a4ee4
Glory Odeyemi <59374178+gloryodeyemi@users.noreply.github.com>
2023-01-04 17:09:10 -0800

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,29 +1,62 @@
 #!/usr/bin/python3
-
-# N queens puzzle
-""""
-    This program solves the N queens problem
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
 """
 
 
-import sys
+from sys import argv
 
-size = len(sys.argv)
-N = sys.argv[1]
-
-def nqueens(size, N)
-    if size != 2:
+if __name__ == "__main__":
+    a = []
+    if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if not isinstance(N, int):
-        ("N must be a number")
+    if argv[1].isdigit() is False:
+        print("N must be a number")
         exit(1)
-    if N < 4:
-        ("N must be at least 4")
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
         exit(1)
-    else:
-        return [
-                y + x
-                for y in nqueens(1, N)
-                for x in nqueens(N + 1, N
-            ]
+
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
+
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
+                return True
+        return False
+
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
+
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
+
+    # start the recursive process at x = 0
+    nqueens(0)
